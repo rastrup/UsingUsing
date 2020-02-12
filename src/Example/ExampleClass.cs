e2f37c4f94836f667b5ee760d13dbd63872f30eb
Kristian Rastrup <kra@copenhagensoftware.com>
2020-02-12 10:42:33 +0100

    Uploading Example

---
+++ src/Example/ExampleClass.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    public class ExampleClass
+    {
+        public void DoStuff()
+        {
+            using (var stream = File.OpenWrite("filename"))
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.Write("content");
+                }
+            }
+        }
+
+        public void DoStuffNew()
+        {
+            using var stream = File.OpenWrite("filename");
+            using var writer = new StreamWriter(stream);
+            writer.Write("content");
+        }
+
+        public async Task DoStuffAsync()
+        {
+            await using (var stream = File.OpenWrite("filename"))
+            {
+                await using (var writer = new StreamWriter(stream))
+                {
+                    writer.Write("content");
+                }
+            }
+        }
+
+        public async Task DoStuffNewAsync()
+        {
+            await using var stream = File.OpenWrite("filename");
+            await using var writer = new StreamWriter(stream);
+            writer.Write("content");
+        }
+    }
+}
